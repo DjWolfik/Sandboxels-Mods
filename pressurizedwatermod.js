d3e0a5b5d47e28273059dcfd8f7a3a191e0e15f7
DjWolfik <djwolf.biznes@gmail.com>
2026-02-26 12:16:34 +0100

    pressurizedwatermod.js

--- pressurizedwatermod.js
+++ pressurizedwatermod.js
@@ -2,7 +2,5 @@ elements.pressurized_water = {
     color: "#0e2c8f",
     behavior: behaviors.LIQUID,
     category: "liquids",
-    state: "liquid",
-    density: 1000,
-    viscosity: 60000,
+    state: "liquid"
 }
