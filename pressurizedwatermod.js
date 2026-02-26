36b3ae81c782eb5782fcb5295405ddab895b6cab
DjWolfik <djwolf.biznes@gmail.com>
2026-02-26 12:24:30 +0100

    Update pressurizedwatermod.js

--- pressurizedwatermod.js
+++ pressurizedwatermod.js
@@ -1,7 +1,15 @@
-elements.pressurizedwater = {
-    color: "#0e2c8f",
+elements.pressurized_water = {
+    color: "#0055cc",
     behavior: behaviors.LIQUID,
     category: "liquids",
-    state: "liquid"
+    state: "liquid",
+    density: 1050,
+    viscosity: 1000,
+    tempHigh: 500,
+    tempHighEl: "steam",
+    tempLow: 0,
+    tempLowEl: "ice",
+    label: "Pressurized Water",
+    desc: "Water under high pressure. Boils only at 500°C.",
 }
 
